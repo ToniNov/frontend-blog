90c116dc0f3d8bb9bb5a2e9730ffb88bbd2e21ab
ToniNov <antoni.novik@yandex.ru>
2022-10-24 20:58:14 +0300

    fix

--- src/axios.js
+++ src/axios.js
@@ -8,6 +8,7 @@ const instance = axios.create({
 instance.interceptors.request.use((config) => {
   config.headers.Authorization = window.localStorage.getItem('token');
   return config;
+  console.log(config)
 });
 
 export default instance;
